57c9b541b8be95e7621e5bc02a33cf506a760557
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2021-02-11 17:22:21 -0800

    Include autoscaling for queue index

--- infra/cloud/db_messages.tf
+++ infra/cloud/db_messages.tf
@@ -78,3 +78,49 @@ resource "aws_appautoscaling_policy" "messages_write_policy" {
   }
 }
 
+resource "aws_appautoscaling_target" "messages_ByQueue_read_target" {
+  max_capacity       = 1000
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.messages.name}/index/ByQueue"
+  scalable_dimension = "dynamodb:index:ReadCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "messages_ByQueue_read_policy" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.messages_ByQueue_read_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.messages_ByQueue_read_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.messages_ByQueue_read_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.messages_ByQueue_read_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+    target_value = 70
+  }
+}
+
+resource "aws_appautoscaling_target" "messages_ByQueue_write_target" {
+  max_capacity       = 1000
+  min_capacity       = 1
+  resource_id        = "table/${aws_dynamodb_table.messages.name}/index/ByQueue"
+  scalable_dimension = "dynamodb:index:WriteCapacityUnits"
+  service_namespace  = "dynamodb"
+}
+
+resource "aws_appautoscaling_policy" "messages_ByQueue_write_policy" {
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.messages_ByQueue_write_target.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.messages_ByQueue_write_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.messages_ByQueue_write_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.messages_ByQueue_write_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+    target_value = 70
+  }
+}
+
