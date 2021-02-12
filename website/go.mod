06ce1349916a378bb0711559e9bdb49d86b71a65
Wolfgang Meyers <wolfgangmeyers@gmail.com>
2021-02-11 18:20:27 -0800

    Restore blockly stuff

---
+++ website/go.mod
@@ -0,0 +1,5 @@
+module bloxserver
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3 // indirect
---
+++ website/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"container/list"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// push to and fetch from queues
+	r := gin.New()
+	r.Static("/ui", "./static")
+	r.Static("/media", "./media")
+	http.ListenAndServe(":9080", r)
+}
